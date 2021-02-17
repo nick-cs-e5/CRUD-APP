18a1e4184f3ca6dc29ef14c7fb3782c1dfc991aa
nick-cs-e5 <79009153+nick-cs-e5@users.noreply.github.com>
2021-02-17 11:10:37 +0530

    Add files via upload

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,27 +1,24 @@
 import { h } from 'preact';
-import { Router } from 'preact-router';
-
-// Code-splitting is automated for `routes` directory
-import Home from '../routes/home';
-import Profile from '../routes/addBranch';
+import { Link, Router } from 'preact-router';
+import Branch from '../routes/Branch/index'; 
 
 const App = () => (
 	<div id="app">
 		<div class="mdl-layout mdl-js-layout mdl-layout--fixed-drawer">
 			<div class="mdl-layout__drawer shadow">
 				<center>
-					<p className="pt-4 mt-1 pb-2" style={{fontSize: 20, color: "#0091ea", fontWeight: "bold"}}>Company Branch</p>
+					<p className="pt-4 pb-2" style={{fontSize: 20, color: "#0091ea", fontWeight: "bold"}}>Company Branch</p>
 				</center>
 				<nav class="mdl-navigation">
-				<a class="mdl-navigation__link shadow-sm" href="/" style={{color: "#000000", fontSize: 16, textDecoration: "none"}}>Home</a>
-				<a class="mdl-navigation__link shadow-sm" href="/branch/add" style={{color: "#000000", fontSize: 16, textDecoration: "none"}}>Add Branch</a>
+				<a class="mdl-navigation__link shadow-sm" href="/" style={{color: "#000000", fontSize: 16, textDecoration: "none"}}>Branches</a>
 				</nav>
 			</div>
 			<main class="mdl-layout__content">
 				<div class="page-content">
+
+				{/* Calling branch in the home page of the dashboard */}
 				<Router>
-					<Home path="/" />
-					<Profile path="/branch/add"/>
+					<Branch path="/" /> 
 				</Router>
 				</div>
 			</main>
